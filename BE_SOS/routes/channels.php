12f754d0f1696b877928fefa635bdb8408d39c51
Tridz <ctris0201@gmail.com>
2026-04-27 15:07:47 +0700

    tri-udbe-3h

--- BE_SOS/routes/channels.php
+++ BE_SOS/routes/channels.php
@@ -11,14 +11,19 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('rescue-requests', function () {
-    return true;
+Broadcast::channel('rescue-requests', function ($user) {
+    // Only authenticated users can listen to general rescue requests channel
+    // This is mainly for admin users to monitor all requests
+    return $user !== null;
 });
 
 Broadcast::channel('rescue-requests.{userId}', function ($user, $userId) {
-    return true;
+    // Users can only listen to their own requests
+    return $user && $user->id == $userId;
 });
 
-Broadcast::channel('rescue-requests.{requestId}', function ($user, $requestId) {
-    return true;
+Broadcast::channel('rescuer-location.{teamId}', function ($user, $teamId) {
+    // Users can listen to location updates of teams assigned to their requests
+    // For now, allow authenticated users - can be refined based on team assignments
+    return $user !== null;
 });
